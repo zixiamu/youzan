bde51450c270ff7b88c8b84743dd719d0511d4be
zixiamu <1214145161@qq.com>
2019-07-07 12:55:41 +0800

    完成详情页下

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -11,6 +11,8 @@ import './goods_custom.css'
 import './goods_mars.css'
 import './goods_sku.css'
 import './goods_theme.css'
+import Swipe from 'components/Swipe.vue'
+import './goods_transition.css'
 
 let {id}=qs.parse(location.search.substr(1))
 
@@ -25,10 +27,17 @@ let detailTab = ['商品详情','本店成交']
 new Vue({
   el:'#app',
   data:{
+    id,
     details:null,
     detailTab,
     tabIndex:0,
-    dealLists:null
+    dealLists:null,
+    bannerLists:[],
+    skuType:1,
+    showSku:false,
+    skuNum:1,
+    isAddCart:false,
+    showAddMessage:false
   },
   created() {
     this.getDetails()
@@ -37,6 +46,13 @@ new Vue({
     getDetails(){
         axios.get(url.details,{id}).then(res=>{
         this.details = res.data.data
+        this.bannerLists = []
+        this.details.imgs.forEach(item=>{
+          this.bannerLists.push({
+            clickUrl:'',
+            img:item
+          })
+        })
       })
     },
     changeTab(index){
@@ -49,10 +65,44 @@ new Vue({
       axios.get(url.deal,{id}).then(res=>{
         this.dealLists = res.data.data.lists
       })
+    },
+    chooseSku(type){
+      this.skuType = type
+      this.showSku =true
+    },
+    changeSkuNum(num){
+      if(num<0&&this.skuNum ===1)return
+      this.skuNum += num
+    },
+    addCart(){
+      axios.post(url.addCart,{
+        id,
+        number:this.skuNum
+      }).then(res=>{
+        if(res.data.status === 200){
+          this.showSku = false
+          this.isAddCart = true
+          this.showAddMessage = true
+          setTimeout(()=>{
+            this.showAddMessage = false
+          },1000)
+        }
+      })
     }
   }
-
-
   ,
-  mixins:[mixin]
-})
+  mixins:[mixin],
+  components:{
+    Swipe
+  },
+  watch:{
+    showSku(val,oldVal){
+      document.body.style.overflow = val? 'hidden':'auto'
+      document.querySelector('html').style.overflow=val?'hidden':'auto'
+      document.body.style.height = val? '100%':'auto'
+      document.querySelector('html').style.height=val?'100%':'auto'
+    }
+  },
+  }
+
+)
